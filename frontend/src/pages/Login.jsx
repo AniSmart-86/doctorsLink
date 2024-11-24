e4575aaeed261b29a58dfa8d371c208adac7a277
Ani Emmanuel <117519788+AniSmart-86@users.noreply.github.com>
2024-11-24 03:28:08 +0100

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
 
             try {
               if (state === 'Sign Up') {
-                const { data } = await axios.post(`${BackendUrl}`, {
+                const { data } = await axios.post(`${BackendUrl}/api/users/register`, {
                   name,
                   email,
                   password,
@@ -48,7 +48,7 @@ const Login = () => {
 
                 
               }else {
-                const {data} = await axios.post(`${BackendUrl}`, {
+                const {data} = await axios.post(`${BackendUrl}/api/users/login`, {
                   email,
                   password,
                 });
